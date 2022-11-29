16b3ab1e175611a709d8124ce85815a5ba1fb715
tharindudarsh <darshanaruh@gmail.com>
2022-11-29 11:55:49 +0530

    second commit

---
+++ src/main/resources/db-script.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user {
+    user_name VARCHAR(50),
+    password VARCHAR(50) NOT NULL,
+    fullName VARCHAR(100) NOT NULL,
+
+    };
+
+CREATE TABLE to-do-item {
+   id VARCHAR(50),
+    user_Name VARCHAR(50) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    status ENUM('DONE','NOT NULL'),
+    PRIMARY KEY(id),
+    FOREIGN KEY (user_Name) REFERENCES user(user_name)
+
+    };
